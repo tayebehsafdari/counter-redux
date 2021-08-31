01b55efe870e88d729bbf5e3e47954b9949e2fa1
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-08-31 12:30:34 +0430

    modify files

--- src/components/counterSlice.js
+++ src/components/counterSlice.js
@@ -7,6 +7,11 @@ import {fetchCount} from "./counterAPI";
     }, 1000);
 }; */
 
+const initialState = {
+    count: 0,
+    status: 'idle'
+};
+
 export const incrementAsync = createAsyncThunk('counter/fetchCount', async (amount) => {
     const response = await fetchCount(amount);
     return response.data;
@@ -14,25 +19,21 @@ export const incrementAsync = createAsyncThunk('counter/fetchCount', async (amou
 
 export const counterSlice = createSlice({
     name: 'counter',
-    initialState: {
-        count: 0
-    },
+    initialState,
     reducers: {
-        increment: state => {
-            state.count += 1;
-        },
-        decrement: state => {
-            state.count -= 1;
-        },
-        reset: (state, action) => {
-            state.count = action.payload
-        },
-        onChange: (state, action) => {
-            state.count = action.payload
-        },
-        incrementByAmount: (state, action) => {
-            state.count += action.payload;
-        }
+        increment: state => state.count += 1,
+        decrement: state => state.count -= 1,
+        reset: (state, action) => state.count = action.payload,
+        onChange: (state, action) => state.count = action.payload,
+        incrementByAmount: (state, action) => state.count += action.payload
+    },
+    extraReducers: builder => {
+        builder
+            .addCase(incrementAsync.pending, state => state.status = 'loading')
+            .addCase(incrementAsync.fulfilled, (state, action) => {
+                state.status = 'idle';
+                state.count = action.payload;
+            });
     }
 });
 
@@ -40,7 +41,7 @@ export const {increment, decrement, reset, onChange, incrementByAmount} = counte
 export const selectCount = state => state.counter.count;
 export default counterSlice.reducer;
 
-console.log("counterSlice: ", counterSlice.actions);
+// console.log("counterSlice: ", counterSlice.actions);
 // console.log("counterSlice: ", counterSlice.actions.increment());
 // console.log("counterSlice: ", counterSlice.reducer({count: 10}, counterSlice.actions.increment()));
 
